4a909ecafb0aa5f033501e08df7c07cde3f054fe
RZA <77061473+rzathere@users.noreply.github.com>
2023-02-02 02:03:30 +0530

    Add files via upload

---
+++ Assets/Rotator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public float speed;
+
+
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, speed)* Time.deltaTime);
+    }
+
+
+}
